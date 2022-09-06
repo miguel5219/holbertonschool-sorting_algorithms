abce991cf35f3f9b3d7c275e7e89720892c05056
miguel5219 <5219@holbertonschool.com>
2022-09-06 15:35:46 -0700

    fix betty coding style and some line of code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,20 +12,20 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, temp, ele_idx = 0;
 
-	if (array == NULL)
+	/*if (array == NULL)
 		return;
 	if (size < 2)
-		return;
+		return; */
 
-	for (; i < size; i++) 
+	for (i = 0; i < size; i++)
 	{
-		for (; ele_idx < size - 1; ele_idx++)
+		for (ele_idx = 0; ele_idx < size - 1; ele_idx++)
 		{
 			if (array[ele_idx] > array[ele_idx + 1])
 			{
 				temp = array[ele_idx];
-				array[ele_idx] = array[ele_idx +1];
-				array[ele_idx +1] = temp;
+				array[ele_idx] = array[ele_idx + 1];
+				array[ele_idx + 1] = temp;
 				print_array(array, size);
 			}
 		}
